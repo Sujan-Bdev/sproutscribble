cdc3a3c82024f5a89ef33c686cde39b6a002f991
Sujan Budhathoki <sujanbudhathoki123@gmail.com>
2024-06-21 18:18:32 +1000

    Add query posts server action

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-  return <div>Hello World</div>;
+import getPosts from '@/server/actions/getPosts';
+
+export default async function Home() {
+  const { error, success } = await getPosts();
+  if (error) {
+    throw new Error(error);
+  }
+  if (success) {
+    return (
+      <main>
+        {success?.map(post => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>{' '}
+          </div>
+        ))}
+      </main>
+    );
+  }
 }
