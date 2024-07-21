86bbc27c0180a8394a62cc4a6d12a0ac115019fe
Sujan Budhathoki <sujanbudhathoki123@gmail.com>
2024-07-21 19:16:59 +1000

    Update drizzle kit config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,16 @@
 import * as dotenv from 'dotenv';
 import type { Config } from 'drizzle-kit';
+import { defineConfig } from 'drizzle-kit';
 
 dotenv.config({
-    path: '.env.local',
-    });
-console.log(process.env.POSTGRES_URL)
-export default {
+  path: '.env.local',
+});
+
+export default defineConfig({
+  dialect: 'postgresql',
   schema: './server/schema.ts',
   out: './server/migrations',
-  //   driver: 'pg',
-  dialect: 'postgresql',
   dbCredentials: {
     url: process.env.POSTGRES_URL!,
   },
-} satisfies Config;
+});
